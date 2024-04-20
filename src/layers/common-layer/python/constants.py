19f5b61072eb1a574e6e67d19ce743008d146958
ii887522 <ii887522@gmail.com>
2024-04-20 18:25:17 +0800

    Get environment variable should not have default value to avoid bug leak to production

--- src/layers/common-layer/python/constants.py
+++ src/layers/common-layer/python/constants.py
@@ -5,11 +5,11 @@ STAGING = "stage"
 PRODUCTION = "prod"
 
 # Environment variables
-LOG_LEVEL = os.environ.get("LOG_LEVEL", "INFO")
-REGION = os.environ.get("REGION", "us-east-1")
-STAGE = os.environ.get("STAGE", STAGING)
-STAGE_PREFIX = os.environ.get("STAGE_PREFIX", "stage_")
-STAGE_DASH_PREFIX = os.environ.get("STAGE_DASH_PREFIX", "stage-")
+LOG_LEVEL = os.environ["LOG_LEVEL"]
+REGION = os.environ["REGION"]
+STAGE = os.environ["STAGE"]
+STAGE_PREFIX = os.environ["STAGE_PREFIX"]
+STAGE_DASH_PREFIX = os.environ["STAGE_DASH_PREFIX"]
 
 # Timestamps
 # Assume that these timestamps are not reachable
